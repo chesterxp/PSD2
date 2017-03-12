e588bb5e39194160d30a4e5a963b1d6e82464e04
chesterxp <chesterxp7@wp.pl>
2017-03-12 03:03:41 +0100

    poprawa wszytkich elementów, dodanie animacji, responsywnosc

--- js/custom.js
+++ js/custom.js
@@ -1,10 +1,22 @@
-$(document).ready(function () {
-//      alert('dom');
-      	$('*[data-animate]').addClass('hidee').each(function () {
-      		$(this).viewportChecker({
-      			classToAdd: 'showw animated ' + $(this).data('animate'),
-      			classToRemove: 'hidee',
-      			offset: '20%'
-      		});
-      	});
+$(document).ready(function(){
+    //smoothScroll
+		$('a').smoothScroll({
+		offset:10,
+		speed:600,
+  
+  
     });
+  
+  //viewportChecker
+  	$('*[data-animate]').addClass('hidee').each(function(){
+      $(this).viewportChecker({
+        classToAdd: 'showw animated ' + $(this).data('animate'),
+        classToRemove: 'hidee',
+        offset: '30%'
+      });
+    });
+  
+  //-------
+  
+  
+});
